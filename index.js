418224eb157366e5995a7fef299350f945742b7c
Orozobekov Dosbol <orozobekovdosbol@gmail.com>
2023-01-18 15:09:59 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+let Name = "Dosbol";
+
+console.log(Name);
+console.log("My name's length is " + Name.length);
+console.log(Name.toLowerCase());
+console.log(Name.toUpperCase());
+
+let pi = 3.141593;
+
+console.log(pi);
+console.log(pi.toFixed(2))
+console.log(pi.toFixed(0))
+
+let example1 = 2 + 2;
+let example2 = 2 - 2;
+let example3 = 2 * 2;
+let example4 = 2 / 2;
+
+let example5 = 2 ** 4; //степень (квадрат)
+let example6 = 81 ** (1/3); //кубический корень
+
+let example7 = 5 % 2; //остаток 
+
+let example8 = "Hello " + "I'm" + `!`;
+
+let example9 = 4 + 10 + 12; //26
+let example10 = 1 + 5 * 3;  //16
+let example11 = 15 * (1 - 1);//0
+
+let example12 = 5 + '3'; //53
+let example13 = '5' + 3; //53
+let example14 = '5' * 3; //15
+let example15 = 'a' * 3; //NaN
+
+let example16 = -15; //делает число негативным
+let example17 = +'15'
+
+
+console.log(example17)
